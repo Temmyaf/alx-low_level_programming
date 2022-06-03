b06d9abff516e21bf889ea467a16013abb886bbc
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-03 10:12:48 -0700

    Last digit correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf(" is %d and is 0\n", n % 10);
 }
 else if (n % 10 < 6)
 {
-printf(" is %d and is less than 6\n", n % 10);
+printf(" is %d and is less than 6 and not 0\n", n % 10);
 }
 return (0);
 }
