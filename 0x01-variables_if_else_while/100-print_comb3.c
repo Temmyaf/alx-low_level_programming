898e434aed4c29304ed9eab64effa8844891d99e
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-03 12:49:57 -0700

    Comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i < 58; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (i != j - 1 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
