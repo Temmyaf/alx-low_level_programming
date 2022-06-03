a2e004f73a7e42695525576b876bf8f46add5e3a
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-03 10:56:54 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry  point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a = 'z'; a++)
+{
+if (a != 'p' || a != 'e')
+{
+putchar(a);
+}
+}
+putchar('\n');
+return (0);
+}
