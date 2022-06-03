4b1c690d12572df231e59067f33e3e4437a58fed
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-03 10:40:10 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+puchar(a);
+}
+putchar('\n');
+}
