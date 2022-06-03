ae9a301e6a3fb1f8635c74bb618cb1a94ec02fc3
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-03 06:44:11 -0700

    Negative pos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Main function
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 int n;
