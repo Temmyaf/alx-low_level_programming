385959540fbf77eef960fbec25bf0e44a8a219a7
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-07 14:32:30 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
-/**
- *main - entry point
- *
- * Return: 0
- */
+#include "holberton.h"
 
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
 {
-printf("_putchar/n");
+char word[8] = "_putchar";
+int i;
+char new_line = '\n';
+
+for (i = 0; i < 8; i++)
+{
+write(1, &word[i], 1);
+}
+write(1, &new_line, 1);
+
 return (0);
 }
