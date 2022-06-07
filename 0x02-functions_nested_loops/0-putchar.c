b5667fd747e6bcd8e9158c4d6f375ac7f4c0b16f
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-07 15:45:29 -0700

    test run

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("_putchar/n");
+
+	return (0);
+}
