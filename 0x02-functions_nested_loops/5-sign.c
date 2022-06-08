1eb4d2edc85d34a404abfc40fbc2a90f676b5378
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-08 04:37:52 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign()- prints the sign of a number
+ * @n: type int
+ * Return: 1 if positive or 0 if zero or -1 if negative
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1);
+}
+else if (n == 0)
+{
+put char('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+}
