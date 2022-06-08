5dcf764f53d56ab4e389b5d16b5ff27e51699629
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-08 06:13:28 -0700

    printf

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print from n to 98.
+ * @n: type int.
+ */
+void print_to_98(int n)
+{
+int i;
+
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+}
