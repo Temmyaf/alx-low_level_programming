47b6893057e1ae46da2cb681c06c7942c5f44b4d
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-08 04:50:23 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs()- compute absolute value of intger
+ * @a: type int
+ * Return: 0
+ */
+int _abs(int a)
+{
+if (a < 0)
+{
+return (a * -1);
+}
+else
+{
+return (a);
+}
+}
