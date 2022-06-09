e791de783073412211dcdbfb9801b71dd3d3ba3b
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-09 12:42:37 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_most_numbers()- print numbers
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if (i != 50 && 1 != 52)
+_putchar(i);
+}
+_putchar('\n');
+}
