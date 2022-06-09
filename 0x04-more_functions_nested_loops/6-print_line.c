4246b244841c66316fc3dabdf0a87db8eb9b5234
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-09 14:07:11 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * print_line - print a line n times
+ * @n: the number of times
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
