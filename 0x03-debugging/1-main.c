cdedf85571fa52ea6215941f5a6996926a70d382
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-09 00:53:00 -0700

    main code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -13,12 +14,12 @@ int main(void)
 
 	i = 0;
 	/**
-	* while (i < 10)
-	*{
-	*      putchar(i);
-	*}
-	*/
-	printf("Infinite loop avoided! \\ o/ \n");
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
