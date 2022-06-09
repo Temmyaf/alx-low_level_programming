a469380a03b33566c464f0fbce57defdb25b8bff
temmyaf <temiloluwa.afolabi@elizadeuniversity.edu.ng>
2022-06-09 11:55:43 -0700

    Include isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit()- check for digit
+ *@c: type int
+ *Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
